a89c6901057ba3681f0eb1021437fc1a3ef655bf
tongjin <tongjin@uqpay.com>
2019-01-28 18:55:34 +0800

    fix: namespace change

--- UqpayConfig.php
+++ UqpayConfig.php
@@ -8,7 +8,7 @@ use uqpay\payment\sdk\models\config\SecureConfig;
 use uqpay\payment\sdk\UqpayApi;
 
 $paygateConfig = new paygateConfig();
-$paygateConfig->apiRoot="http://localhost:8080/";
+$paygateConfig->apiRoot="http://paygate.uqpay.cn";
 $paygateConfig->testMode = true;
 $paygateConfig->testRSA=new SecureConfig(['publicKeyPath'=>'UQPAY_pub.pem','privateKeyPath'=>'MerchantID_prv.pem']);
 $merchantConfig = new merchantConfig();
